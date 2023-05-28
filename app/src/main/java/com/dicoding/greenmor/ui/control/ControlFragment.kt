204afeb05f5baa619b548f2f5572fb9cd8a1fe5b
Rizky Fadillah <fadillahrizki16@gmail.com>
2023-05-28 12:00:04 +0700

    add switch

--- app/src/main/java/com/dicoding/greenmor/ui/control/ControlFragment.kt
+++ app/src/main/java/com/dicoding/greenmor/ui/control/ControlFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.dicoding.greenmor.R
 import com.dicoding.greenmor.databinding.FragmentControlBinding
 
 class ControlFragment : Fragment() {
@@ -25,6 +26,43 @@ class ControlFragment : Fragment() {
 
         _binding = FragmentControlBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        binding.switchLamp.setOnCheckedChangeListener{_, isChecked ->
+            if(isChecked){
+                binding.statusLamp.text = "On"
+                binding.statusLamp.setTextColor(getResources().getColor(R.color.teal_200))
+            } else {
+                binding.statusLamp.text = "Off"
+                binding.statusLamp.setTextColor(getResources().getColor(R.color.danger))
+            }
+        }
+        binding.switchFan.setOnCheckedChangeListener{_, isChecked ->
+            if(isChecked){
+                binding.statusFan.text = "On"
+                binding.statusFan.setTextColor(getResources().getColor(R.color.teal_200))
+            } else {
+                binding.statusFan.text = "Off"
+                binding.statusFan.setTextColor(getResources().getColor(R.color.danger))
+            }
+        }
+        binding.switchPump.setOnCheckedChangeListener{_, isChecked ->
+            if(isChecked){
+                binding.statusPump.text = "On"
+                binding.statusPump.setTextColor(getResources().getColor(R.color.teal_200))
+            } else {
+                binding.statusPump.text = "Off"
+                binding.statusPump.setTextColor(getResources().getColor(R.color.danger))
+            }
+        }
+        binding.switchWater.setOnCheckedChangeListener{_, isChecked ->
+            if(isChecked){
+                binding.statusWater.text = "On"
+                binding.statusWater.setTextColor(getResources().getColor(R.color.teal_200))
+            } else {
+                binding.statusWater.text = "Off"
+                binding.statusWater.setTextColor(getResources().getColor(R.color.danger))
+            }
+        }
 
         return root
     }
